681804fdee6ded6c80323ec088a69cd4aa28c742
SurienDG <surien.das-giwojno@uwaterloo.ca>
2020-05-04 12:47:44 -0400

    change to only take in promise

--- promiseWrapper.cc
+++ promiseWrapper.cc
@@ -4,19 +4,31 @@
 #include <functional>
 #include <memory>
 #include <thread>
+
 #include "napi-thread-safe-callback.hpp"
 
-void promiseFuncWrapper(const Napi::Function &resolveInput, const Napi::Function &rejectInput, const PromiseFunc &promFunc)
-{
+Napi::Promise promiseFuncWrapper(const Napi::Env &env,
+                                 const PromiseFunc &promFunc) {
+    Napi::Promise::Deferred promise = Napi::Promise::Deferred::New(env);
+    Napi::Function resolveInput =
+        Napi::Function::New(env, [](const Napi::CallbackInfo& info) {
+            Napi::HandleScope scope(info.Env());
+        });
+    Napi::Function rejectInput =
+       Napi::Function::New(env, [promise](const Napi::CallbackInfo& info) {
+                promise.Reject(info[0].As<Napi::String>());
+        });
     std::shared_ptr<ThreadSafeCallback> resolveInputPtr =
         std::make_shared<ThreadSafeCallback>(resolveInput);
     std::shared_ptr<ThreadSafeCallback> rejectInputPtr =
         std::make_shared<ThreadSafeCallback>(rejectInput);
 
     // create resolve function
-    resolveFunc resolve = [resolveInputPtr](const std::string argsInput) {
+    resolveFunc resolve = [resolveInputPtr, promise](const std::string argsInput) {
         resolveInputPtr->call(
-            [argsInput](Napi::Env env, std::vector<napi_value> &args) {
+            [&argsInput, &promise](Napi::Env env, std::vector<napi_value> &args) {
+              //  Napi::HandleScope scope (env);
+              //  promise.Resolve(Napi::String::New(env, argsInput));
                 // This will run in main thread and needs to construct the
                 // arguments for the call
                 args = {Napi::String::New(env, argsInput)};
@@ -28,4 +40,6 @@ void promiseFuncWrapper(const Napi::Function &resolveInput, const Napi::Function
     };
     // call our function to do work and either resolve or reject a resolution
     promFunc(resolve, reject);
+
+    return promise.Promise();
 }
