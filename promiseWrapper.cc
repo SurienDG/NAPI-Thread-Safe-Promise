68a9ffa08d61e7e6f8437f5c8f0b6ee05f1bce8a
SurienDG <surien.das-giwojno@uwaterloo.ca>
2020-05-05 01:41:19 -0400

    use napi thread safe function

--- promiseWrapper.cc
+++ promiseWrapper.cc
@@ -1,31 +1,59 @@
-
 #include "promiseWrapper.h"
 
 #include <functional>
 #include <memory>
 #include <thread>
-#include "napi-thread-safe-callback.hpp"
 
-void promiseFuncWrapper(const Napi::Function &resolveInput, const Napi::Function &rejectInput, const PromiseFunc &promFunc)
-{
-    std::shared_ptr<ThreadSafeCallback> resolveInputPtr =
-        std::make_shared<ThreadSafeCallback>(resolveInput);
-    std::shared_ptr<ThreadSafeCallback> rejectInputPtr =
-        std::make_shared<ThreadSafeCallback>(rejectInput);
+struct tfsnContext {
+    Napi::Promise::Deferred deffered;
+    std::string data;
+    bool resolve;
+    Napi::ThreadSafeFunction tsfn;
+    tfsnContext(Napi::Env env)
+        : deffered{Napi::Promise::Deferred::New(env)},
+          data{""},
+          resolve{false} {};
+};
+
+void FinalizerCallback(Napi::Env env, void *finalizeData, void *context) {}
+
+Napi::Promise promiseFuncWrapper(const Napi::Env env,
+                                 const PromiseFunc &promFunc) {
+    std::shared_ptr<tfsnContext> context = std::make_shared<tfsnContext>(env);
+    std::shared_ptr<std::mutex> mu = std::make_shared<std::mutex>();
+    context->tsfn = Napi::ThreadSafeFunction::New(
+        env, Napi::Function::New(env, [](const Napi::CallbackInfo &info) {}),
+        "TSFN", 0, 1, [context, mu](Napi::Env env) {
+            mu->lock();
+            if (context->resolve) {
+                context->deffered.Resolve(
+                    Napi::String::New(env, context->data));
+            } else {
+                context->deffered.Reject(
+                    Napi::String::New(env, context->data));
+            }
+            mu->unlock();
+        });
 
     // create resolve function
-    resolveFunc resolve = [resolveInputPtr](const std::string argsInput) {
-        resolveInputPtr->call(
-            [argsInput](Napi::Env env, std::vector<napi_value> &args) {
-                // This will run in main thread and needs to construct the
-                // arguments for the call
-                args = {Napi::String::New(env, argsInput)};
-            });
+    resolveFunc resolve = [context, mu](const std::string argsInput) {
+        mu->lock();
+        context->data = argsInput;
+        context->resolve = true;
+        mu->unlock();
+        context->tsfn.Release();
     };
     // create reject function
-    rejectFunc reject = [rejectInputPtr](const std::string &msg) {
-        rejectInputPtr->callError(msg);
+    rejectFunc reject = [context, mu](const std::string &msg) {
+        mu->lock();
+        context->data = msg;
+        context->resolve = false;
+        mu->unlock();
+        context->tsfn.Release();
     };
+
     // call our function to do work and either resolve or reject a resolution
     promFunc(resolve, reject);
+
+    return context->deffered.Promise();
 }
--- promiseWrapper.h
+++ promiseWrapper.h
@@ -15,18 +15,11 @@ typedef std::function<void(resolveFunc, rejectFunc)> PromiseFunc;
 
 // this will give the promise function (promFunc) the resolve and reject
 // functions the promise function can then be used to do the main work
-void promiseFuncWrapper(const Napi::Function &resolveInput,
-                        const Napi::Function &rejectInput,
-                        const PromiseFunc &promFunc);
+Napi::Promise promiseFuncWrapper(const Napi::Env env,
+                                 const PromiseFunc &promFunc);
 
-#define PROMISE(info, funcBody)                                             \
-    if (info.Length() < 2) {                                                \
-        throw Napi::Error::New(info.Env(),                                  \
-                               "requires at least two function arguments"); \
-    }                                                                       \
-    promiseFuncWrapper(info[info.Length() - 2].As<Napi::Function>(),        \
-                       info[info.Length() - 1].As<Napi::Function>(),        \
-                       [&](resolveFunc resolve, rejectFunc reject)      \
-                           funcBody)
+#define PROMISE(info, funcBody) \
+    promiseFuncWrapper(         \
+        info.Env(), [&](resolveFunc resolve, rejectFunc reject) funcBody)
 
 #endif
